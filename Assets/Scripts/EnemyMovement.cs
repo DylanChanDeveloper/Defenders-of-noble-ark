be412199850c98d047cc3e7c547e6ed34afd6aa1
DylanChan2121 <dchan2121@gmail.com>
2022-03-30 15:05:39 +0100

    commit 6
    enemy objects move to specified tile location

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    [SerializeField] float waitTimer = 1f;//used to tell how many seconds the co routine will wait for.
+
     [SerializeField] List<Waypoint> myPathWay = new List<Waypoint>();//List syntax is we declare the list with sheverons, 
                                                                      //then inside the cheverons we specifiy the thing its goint to be storing in this case the waypoint script,
                                                                      //we finally initalize the list with new List<Waypoints>, e.g. we initalize pathway to a new list of waypoints
     void Start()
-    {//we are goint to print a list of the waypoint object names into the console
-        PrintWayPoint();
+    {
+        StartCoroutine(PrintWayPoint());//using a co routine so for loop will wait for x amount of seconds before executing. To prevent object from teleporting from point A to B instantly
     }
 
-    void PrintWayPoint()
+    IEnumerator PrintWayPoint()
     {
-        foreach (Waypoint myWaypoint in myPathWay)
+        foreach (Waypoint myWaypoint in myPathWay)//loops through every element in the list
         {
-            Debug.Log(myWaypoint.name);//will print out the name of the waypoint
+            transform.position = myWaypoint.transform.position;//the first transform.position is on the root of our enemy object we are reassigning the enemy object position to be the waypoints position.
+            yield return new WaitForSeconds(waitTimer);// we are using yield return because for co routines it means give up control and then in this case come back after 1 second. yield is needed when returning a co routine.
         }
     }
 
