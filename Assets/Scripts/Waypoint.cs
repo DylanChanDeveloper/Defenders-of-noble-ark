3fc827222c0516dd0406f89ceff7ddb96763a89e
DylanChan2121 <dchan2121@gmail.com>
2022-04-04 16:23:00 +0100

    commit 10
    enabled mouse input with onMouseDown method and enabled a bool flag to check if tiles can have towers be placed on them, with if statement.

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-
+    [SerializeField] bool canBePlaced;//did not delcare true or false because we set if the object bool is true in the inspector
+     void OnMouseDown()//is called when the user has pressed tge mouse button while over a collider.
+    {
+        if(canBePlaced)//if the object bool canBePlaced is ticked in the inspector than it will run the code below.
+        {           
+            Debug.Log(transform.name);
+        }
+        
+    }
 
 }
