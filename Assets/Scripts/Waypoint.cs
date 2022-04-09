462d6602c5a1a1373b1a37c97155ba2bdd948e01
DylanChan2121 <dchan2121@gmail.com>
2022-04-09 20:42:59 +0100

    commit 12
    enabled toggleable labels with the c key using the get property method.

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class Waypoint : MonoBehaviour
 {
     [SerializeField] GameObject prefabTower;
-    [SerializeField] bool canBePlaced;//did not delcare true or false because we set if the object bool is true in the inspector
+    [SerializeField] bool canBePlaced;//did not delcare true or false because we set if the object bool is true in the inspector.
+    
+    public bool CanBePlaced//this is a property method notice that there is no "()" vs a normal method that would have parmeter brackets.property being used in myCoOrdinateLabeler
+    {  get{  return canBePlaced;   }   }
+
+
+  
      void OnMouseDown()//is called when the user has pressed tge mouse button while over a collider.
     {
         if(canBePlaced)//if the object bool canBePlaced is ticked in the inspector than it will run the code below.
