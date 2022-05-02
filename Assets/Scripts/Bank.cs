af33fd32d995eec096f66242b411d48796b81faa
DylanChan2121 <dchan2121@gmail.com>
2022-05-02 21:15:44 +0100

    commit 18
    added the in UI to update depending on gold

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
@@ -9,9 +10,18 @@ public class Bank : MonoBehaviour
     
     [SerializeField] int currentBalance;
 
-     void Awake()
+    [SerializeField] TextMeshProUGUI displayBalance;
+    
+
+     void Start()
+    {
+           
+    }
+
+    void Awake()
     {
         currentBalance = startingBalance;
+        DisplayUpdate();
     }
 
     public int CurrentBalance {get { return currentBalance; } }//we can access our current balance from outside the script(read) but we cant set it.
@@ -21,16 +31,24 @@ public class Bank : MonoBehaviour
         
         currentBalance += Mathf.Abs (amount);//returns the absolute value, if we pass in a negitive number it will be turned to a positive number and then added to our bank account.
         //currentBalance = currentBalance + amount; were depositing so were adding to are current account.
+
+        DisplayUpdate();
     }
 
     public void withdraw(int amountTwo)
     {
         currentBalance -= Mathf.Abs(amountTwo); //will do the same if its 10 it will withdrawn 10 from our accpount, but if it is -10 mathf.abs will convert it to 10 and minus it from our account.
-       
-        if(currentBalance < 0)
+        DisplayUpdate();
+
+        if (currentBalance < 0)
         {
             reloadScene(); //lose game;
         }
+    }
+
+    void DisplayUpdate()
+    {
+        displayBalance.text = "Gold: " + currentBalance;
     }
 
     void reloadScene()
